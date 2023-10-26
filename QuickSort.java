37de182b72a0d15501900168a812502bc4b1d654
ClaytonStealth <claytonses@yahoo.com>
2023-10-26 16:51:02 -0500

    After reviewing and watching some tutorials i changed the original algo to something more basic and easier to explain and understand

--- QuickSort.java
+++ QuickSort.java
@@ -3,35 +3,59 @@ public class QuickSort {
         //intializing array of random numbers
         int[] array = {12,4,5,6,17,3,1,7};
         //quickSort is main sorting function for the array
-        quickSort(array, 0, array.length - 1);
-        System.out.println("Sorted");
+        System.out.println("Before sorting:");
+        //for each loop/ printing each element as it loops through the array being assigned to value before sorting
         for (int value : array) {
-            System.out.println(value + " ");
+            System.out.println(value + " "); //printing sorted array
+        }
+        quickSort(array, 0, array.length - 1); //calling quickSort function
+        System.out.println("\nSorted");
+        //for each loop/ printing each element as it loops through the array being assigned to value after sorting
+        for (int value : array) {
+            System.out.println(value + " "); //printing sorted array
         }
     }
-//quickSort checks if the low index is less than the high index
+//The main sorting function for the array
     private static void quickSort(int[] array, int lowIndex, int highIndex) {
+        //check if the low index is greater than or equal to the high index
         if (lowIndex >= highIndex) {
-            return;
+            return; //nothing to sort or one element left
         }
-
+        //choose the pivot as the element at the high index
         int pivot = array[highIndex];
-        int leftPointer = lowIndex;
-        int rightPointer = highIndex;
-        while (leftPointer<rightPointer) {
-            while(array[leftPointer]<= pivot && leftPointer < rightPointer) {
-                leftPointer++;
-            }
-            while (array[rightPointer]>= pivot && leftPointer<rightPointer) {
-                rightPointer--;
-            }
-            swap(array, leftPointer, rightPointer);
-
-        }
-        swap(array, leftPointer, highIndexs);
+        //get the index where the pivot should be placed
+        int leftPointer = partition(array, lowIndex, highIndex, pivot);
+        //recursively sort the left and right subarrays
         quickSort(array,lowIndex, leftPointer -1);
         quickSort(array, leftPointer +1, highIndex);
     }
+    //function to partition the array and return the pivot's correct position
+    private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
+        //initializing left and right pointers
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        //loop until the left pointer is less than the right pointer
+        while (leftPointer<rightPointer) {
+            //find an element on the left side that is greater than the pivot
+            while(array[leftPointer]<= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+            //find an element on the right side that is less than the pivot
+            while (array[rightPointer]>= pivot && leftPointer<rightPointer) {
+                rightPointer--;
+            }
+            // swap the elements at leftPointer and rightPointer
+            swap(array, leftPointer, rightPointer);
+
+        }
+        // Swap the pivot element with the element at the leftPointer
+        swap(array, leftPointer, highIndex);
+
+        // Return the correct position of the pivot
+        return leftPointer;
+    }
+    //function to swap two elements in the array
     private static void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
